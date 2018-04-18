531ca3dab01451bf10d418dee147dd9f5db4f3c1
Robbie Marcelo <rbmrclo@hotmail.com>
2018-04-18 20:25:46 +0800

    Updates spec_helper for dependency updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,16 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
-require 'octofart'
+require "octofart"
+require "webmock/rspec"
+
+Dir[File.expand_path("support/**/*.rb", __dir__)].each { |f| require f }
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
   end
 end
