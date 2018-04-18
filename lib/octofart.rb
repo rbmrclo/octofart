4558dffa75139dbe4231b40e2b89b97782c3813a
Robbie Marcelo <rbmrclo@hotmail.com>
2018-04-18 20:26:11 +0800

    Implements Octofart.client

--- lib/octofart.rb
+++ lib/octofart.rb
@@ -1,10 +1,19 @@
+require "octokit"
 require "octofart/version"
 require "octofart/config"
+require "octofart/client"
 
 module Octofart
   extend SingleForwardable
 
-  def_delegators :config, :base_branch, :github_token
+  def_delegators :config, :base_branch, :github_token, :max_retries
+
+  def self.client
+    @client ||= Client.new(
+       max_retries: max_retries,
+      access_token: github_token,
+    )
+  end
 
   def self.configure(&blk)
     yield(config)
