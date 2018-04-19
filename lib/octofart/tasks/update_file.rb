5be577ce1e6b8bbc9ef508ea6a967c59a6a03256
Robbie Marcelo <rbmrclo@hotmail.com>
2018-04-20 02:00:53 +0800

    Update file task

---
+++ lib/octofart/tasks/update_file.rb
@@ -0,0 +1,22 @@
+module Octofart
+  module Tasks
+    class UpdateFile
+
+      def run(params)
+        params[:repositories].each_pair do |repo_name, metadata|
+          metadata[:tasks].each do |task|
+            file_info   = Octofart.client.contents(repo_name, { path: task[:path], branch: metadata[:base_branch][:name]})
+            old_content = Base64.decode64(file_info.content)
+            new_content = old_content.gsub(task[:find], task[:replace])
+
+            task[:blob_sha] = Octofart.client.create_blob(repo_name, Base64.encode64(new_content), "base64")
+
+            puts "Updating file from #{old_content} to #{new_content}..."
+          end
+        end
+
+        params
+      end
+    end
+  end
+end
